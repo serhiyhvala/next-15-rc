2ae316999a60b7b67daeb8ec59e2a646ead93a5a
Sergiy Khvala <khvala@macpaw.com>
2024-05-27 23:10:53 +0300

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./src/app/**/*.{ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {},
   plugins: [],
 };
