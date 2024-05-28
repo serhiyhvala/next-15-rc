41d4fcdbb15651fe154168c1864b6014efc885f7
Sergiy Khvala <khvala@macpaw.com>
2024-05-28 09:12:47 +0300

    feat: create after example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { getTime } from "@/actions";
+import { unstable_after as after } from "next/server";
 
 // export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const time = await getTime();
+
+  after(() => {
+    console.log("after");
+  });
 
   return (
     <main>
